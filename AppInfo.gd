e5d6468d5f290993fed7833db01cb25d7a9e3aa1
Oğuzhan Eroğlu <rohanrhu2@gmail.com>
2025-08-25 06:02:51 +0300

    Bump v0.11.0-git

--- AppInfo.gd
+++ AppInfo.gd
@@ -5,8 +5,8 @@
 extends Node
 class_name AppInfo
 
-static var VERSION = [0, 10, 0, "alpha"]
-static var VERSION_STR = "v0.10.0-alpha"
+static var VERSION = [0, 11, 0, "git"]
+static var VERSION_STR = "v0.11.0-git"
 
 func _ready() -> void:
 	pass
